8a7b7c52678792bf1e4aef0fddeb81e249131f09
binaris00 <binaris000@gmail.com>
2023-05-16 22:28:49 -0300

    first conditionals practice

---
+++ Conditionals/Practiceelseif.java
@@ -0,0 +1,32 @@
+package Conditionals;
+
+public class Practiceelseif {
+   public static void main(String[] args){
+      int number = 9;
+
+      if (number == 1){
+         System.out.println("number is 1");
+      }
+      
+      else if (number == 2){
+         System.out.println("number is 2");
+      }
+
+      else if (number == 3){
+         System.out.println("number is 3");
+      }
+
+      else if (number == 4){
+         System.out.println("number is 4");
+      }
+
+      else if (number == 5){
+         System.out.println("number is 5");
+      } 
+
+      else {
+         System.out.println("number is not 1, 2, 3, 4, 5");
+      }
+   
+   }
+}
---
+++ Conditionals/Practiceif.java
@@ -0,0 +1,19 @@
+package Conditionals;
+
+public class Practiceif {
+   public static void main(String[] args){
+      String text = "";
+
+      if (text == "Hello World"){
+         System.out.println("Yes this is a hello word string!");
+      }
+
+      else if (text == ""){
+         System.out.println("Yes this is an empty string!");
+      }
+
+      else{
+         System.out.println("No this is not a hello word string! D:");
+      }
+   }
+}
