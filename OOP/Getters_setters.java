9658a13e019b58dc76bec5cdf26ca5c535c6257b
binaris00 <binaris000@gmail.com>
2023-05-18 19:46:52 -0300

    Simples Getters and setters

---
+++ OOP/Getters_setters.java
@@ -0,0 +1,24 @@
+package OOP;
+
+public class Getters_setters {
+
+   private String name;
+   private int age;
+   private double weight;
+   private String gender;
+
+
+   public String getName() { return name; }
+   public void setName(String name) {this.name = name;}
+
+   public int getAge() {return age;}
+   public void setAge(int age) {this.age = age;}
+
+   public double getWeight() {return weight;}
+   public void setWeight(double weight) {this.weight = weight;}
+
+   public String getGender() {return gender;}
+   public void setGender(String gender) {this.gender = gender;}
+
+   
+}
