3d0305d4e074472bb2f0b15734d4179a6b47e079
binaris00 <binaris000@gmail.com>
2023-05-19 18:15:27 -0300

    Fixed the scanner

--- 1-First steps (Data Types and methods)/Userinput.java
+++ 1-First steps (Data Types and methods)/Userinput.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 
 public class Userinput {
    public static void main(String[] args){
-      Scanner scanner = new Scanner(System.in);
+      try (Scanner scanner = new Scanner(System.in)) {
+         System.out.println("Can you say me your name?");
+         String user = scanner.nextLine();
 
-      System.out.println("Can you say me your name?");
-      String user = scanner.nextLine();
-
-      System.out.println("Hello " + user);
+         System.out.println("Hello " + user);
+      }
    }
 }
