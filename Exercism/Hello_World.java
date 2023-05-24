f3b4da9db82f3ae323a9d641c4c4a26343ab137d
binaris00 <binaris000@gmail.com>
2023-05-24 17:15:09 -0300

    Practice

---
+++ Exercism/Hello_World.java
@@ -0,0 +1,18 @@
+package Exercism;
+/*
+ * This exercise is from
+ * https://exercism.org/tracks/java/exercises/hello-world
+ * 
+ * If you want to see the original exercise you just need to check the link
+ */
+
+
+public class Hello_World {
+   static String hello = "Hello World";
+   public static void main(String[] args) {
+      //Only the print
+      System.out.println("Hello World");
+      
+      System.out.println(hello);
+   }
+}
