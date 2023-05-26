1d259abfed31d3e051ced96a20e53710252599ae
binaris00 <binaris000@gmail.com>
2023-05-26 20:12:16 -0300

    For

---
+++ 1-First steps (Data Types and methods)/Multiplicationtable.java
@@ -0,0 +1,17 @@
+public class Multiplicationtable{
+   public static void main(String[] args) {
+      for (int first=1;first<=10;first++){
+
+         if (first != 1){System.out.println("");}
+
+
+         System.out.println("Table of "+first);
+         for (int second=1;second<=10;second++){
+            
+            for (int result=0;result<1;result++){
+               System.out.println(first+"x"+second+"= "+first*second);
+            }
+         }
+      }
+   }
+}
---
+++ DataStructures/Buclefor.java
@@ -0,0 +1,22 @@
+package DataStructures;
+
+public class Buclefor {
+   public static void main(String[] args) {
+      //Simple for to print number 1 to 10
+      for (int i=1;i<=10;i++){
+         System.out.println(i);
+      }
+
+      
+      for (int number=1;number<=10;number++){
+         System.out.println("6x" + number + "= " + 6*number);
+      }
+      
+      int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8 , 9, 10};
+
+      for (int number:numbers){
+         System.out.println(number);
+      }
+      
+   }
+}
